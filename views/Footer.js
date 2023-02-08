803a06f228c4ce823236fac960a82df2b5c933bd
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2023-02-08 21:40:53 +0600

    💅🛠️footer style updated

--- views/Footer.js
+++ views/Footer.js
@@ -1,29 +1,29 @@
 import Image from "next/image";
 import React from "react";
-import { Col, Row } from "react-bootstrap";
 import SocialIcons from "../components/SocialIcons";
 
 export default function Footer() {
   return (
     <footer className="container-fluid bg-gray-700">
       <div className="container-xl p-3 pt-4">
-        <Row>
-          <Col
-            className="d-flex flex-column flex-sm-row align-items-center gap-4"
-            style={{ width: "max-content" }}
-          >
+        <div className="d-grid footer-grid">
+          <div className="d-flex  my-3 my-lg-0">
             <div
-              className="position-relative"
-              style={{ width: "min(89px,100%)", aspectRatio: "89/47" }}
+              className="position-relative mx-auto mx-sm-0"
+              style={{ width: "min(100%,89px)", aspectRatio: "89/47" }}
             >
               <Image layout="fill" src="/logo.svg" alt="artists4web3 logo" />
             </div>
-            <p className="caption-2 my-auto" style={{ width: "max-content" }}>
+          </div>
+          <div className="d-flex align-items-center justify-content-center justify-content-sm-end justify-content-lg-start my-3 my-lg-0">
+            <p className="caption-2 m-0" style={{ width: "max-content" }}>
               &copy; AsGuard 2022 - All rights reserved
             </p>
-          </Col>
-          <Col className="d-flex flex-column flex-sm-row gap-2 align-items-center justify-content-center justify-content-sm-end mt-4 mt-sm-0 ms-auto">
+          </div>
+          <div className="d-flex align-items-center justify-content-center justify-content-sm-start justify-content-lg-end  my-3 my-lg-0">
             <SocialIcons />
+          </div>
+          <div className="d-flex flex-column flex-sm-row gap-2 align-items-center justify-content-center justify-content-sm-end  my-3 my-lg-0">
             <p className="caption2 my-auto ms-0 ms-sm-3">An Initiative by </p>
             <div
               className="position-relative"
@@ -35,8 +35,8 @@ export default function Footer() {
                 alt="MantleLabs Logo"
               />
             </div>
-          </Col>
-        </Row>
+          </div>
+        </div>
       </div>
     </footer>
   );
