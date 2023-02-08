edc923476f183f34be0f2400f4249889f700e76b
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2023-02-08 15:17:19 +0600

    💅🛠️🏷️📕🦄created & completed

---
+++ components/SocialIcons.js
@@ -0,0 +1,49 @@
+import React from "react";
+import {
+  TbBrandTelegram,
+  TbBrandTwitter,
+  TbMail,
+  TbBrandInstagram,
+} from "react-icons/tb";
+
+export default function SocialIcons() {
+  const Socials = [
+    {
+      link: "https://twitter.com/AssetMantle",
+      font: "body1",
+      icon: <TbBrandTwitter />,
+    },
+    {
+      link: "mailto:hello@assetmantle.one",
+      font: "body1",
+      icon: <TbMail />,
+    },
+    {
+      link: "https://t.me/assetmantlechat",
+      font: "body1",
+      icon: <TbBrandTelegram />,
+    },
+    {
+      link: "https://www.instagram.com/assetmantle/",
+      font: "body1",
+      icon: <TbBrandInstagram />,
+    },
+  ];
+  return (
+    <div className="d-flex gap-3 align-items-center justify-content-center">
+      {React.Children.toArray(
+        Socials.map((social) => (
+          <a
+            href={social.link}
+            target="_blank"
+            rel="noreferrer"
+            className={`text-white  ${social.font}`}
+            style={{ lineHeight: "100% !important" }}
+          >
+            {social.icon}
+          </a>
+        ))
+      )}
+    </div>
+  );
+}
