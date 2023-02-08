2a4916e49b26a19c04c646bf48474dceaca78d9c
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2023-02-08 15:38:29 +0600

    🛠️section excluded

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,8 @@
 import Hero from "../views/Hero";
 import About from "../views/About";
-import Artists from "../views/Artists";
+// import Artists from "../views/Artists";
 import VisionMission from "../views/VisionMission";
-import Gallery from "../views/Gallery";
+// import Gallery from "../views/Gallery";
 import Events from "../views/Events";
 import GetInTouch from "../views/GetInTouch";
 
@@ -11,9 +11,9 @@ const IndexPage = () => {
     <main>
       <Hero />
       <About />
-      <Artists />
+      {/* <Artists /> */}
       <VisionMission />
-      <Gallery />
+      {/* <Gallery /> */}
       <Events />
       <GetInTouch />
     </main>
