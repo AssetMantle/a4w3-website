1cd39346fcae2d405aa902826eb93e75576a21e8
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2023-02-10 21:32:41 +0600

    💅🛠️📕new design implemented

--- views/VisionMission.js
+++ views/VisionMission.js
@@ -14,30 +14,49 @@ export default function VisionMission() {
   return (
     <section className="container-xl py-5 my-5 px-3">
       <h1 className="h1 text-uppercase text-center">Vision</h1>
-      <p
-        className="body1 text-center mx-auto my-3"
-        style={{ maxWidth: "880px" }}
-      >
-        Artist4Web3 is meaning to DEMOCRATIZE ART. Art should have no border.
-        With Web3 artist can leveraging global audiences and distribute
-        Censorship-resistant content.
-      </p>
-      <div className="d-flex flex-column flex-md-row align-items-center justify-content-around gap-3 mt-5 pt-4">
-        {React.Children.toArray(
-          visions.map((vision) => (
-            <div className="d-flex flex-column" style={{ maxWidth: "294px" }}>
-              <div
-                className="position-relative mx-auto mb-5"
-                style={{ width: "min(166px, 90%)", aspectRatio: "166/160" }}
-              >
-                <img src={`/vision/${vision.icon}`} alt={vision.content} />
+      <div className="row  mt-5 pt-4">
+        <div className="col-12 col-sm-5 d-flex flex-column gap-4 justify-content-center mb-4 mb-sm-0 pb-5 pb-sm-0">
+          <p
+            className="h2 m-0"
+            style={{ maxWidth: "496px", fontWeight: "300" }}
+          >
+            Artist4Web3 is meaning to DEMOCRATIZE ART.
+          </p>
+          <p
+            className="h2 m-0"
+            style={{ maxWidth: "496px", fontWeight: "300" }}
+          >
+            Art should have no border. With Web3, artists can leverage global
+            audiences and distribute censorship-resistant content .
+          </p>
+        </div>
+        <div className="col-12 col-sm-7 d-flex flex-column flex-md-row align-items-center justify-content-around gap-3">
+          {React.Children.toArray(
+            visions.map((vision) => (
+              <div className="d-flex flex-column" style={{ maxWidth: "294px" }}>
+                <div
+                  className="position-relative mx-auto mb-5"
+                  style={{
+                    width: "min(145px, 90%)",
+                    aspectRatio: "166/160",
+                  }}
+                >
+                  <img
+                    src={`/vision/${vision.icon}`}
+                    alt={vision.content}
+                    style={{
+                      width: "100%",
+                      height: "auto",
+                    }}
+                  />
+                </div>
+                <p className="text-uppercase text-center body2">
+                  {vision.content}
+                </p>
               </div>
-              <p className="text-uppercase text-center body2 text-primary">
-                {vision.content}
-              </p>
-            </div>
-          ))
-        )}
+            ))
+          )}
+        </div>
       </div>
     </section>
   );
